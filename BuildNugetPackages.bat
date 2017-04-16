e84489cb9cc90f37c82d24e72749ba5296300e01
Travis Taylor <codenesium@gmail.com>
2017-04-16 03:30:31 -0500

    nuget publish changes

--- BuildNugetPackages.bat
+++ BuildNugetPackages.bat
@@ -1,3 +1,3 @@
-nuget pack JWTSessionAuth/JWTSessionAuth.csproj  -IncludeReferencedProjects  -o  ../NugetPackages
+nuget pack JWTSessionAuth/JWTSessionAuth.csproj  -o  ../NugetPackages
 nuget pack JWTTool/JWTTool.csproj  -IncludeReferencedProjects  -o  ../NugetPackages
 
